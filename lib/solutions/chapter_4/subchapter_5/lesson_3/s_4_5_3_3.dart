7c73b422102ea74bf068533a7612575f40f83042
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-20 15:55:46 +0200

    had many problems with SDK, no Device available, ...

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -1,9 +1,29 @@
+/*Center in Column",
+  Erstelle einen blauen `Container` mit den Abmessungen 150 x 150.
+Integriere darin eine `Column` mit zwei `Text`-Elementen: `App` und `Akademie`.
+Zentriere beide Texte innerhalb des Containers. */
+
 import 'package:flutter/material.dart';
 
 class S4533 extends StatelessWidget {
   const S4533({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+        return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(height: 150, width: 150, color: Colors.blue),
+              const Text("App"),
+              const Text("Akademie"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+// zentriert NICHT die Schrift in den Container
